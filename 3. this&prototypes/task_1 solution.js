12c54b18439922df91339fe7d8e2e46dba149317
Christina Landvytovych <Crofty.land@gmail.com>
2020-02-13 17:26:14 +0200

    solution 1st task

---
+++ 3. this&prototypes/task_1 solution.js
@@ -0,0 +1,26 @@
+const possibleAllergies = [
+    'eggs',
+    'peanuts',
+    'shellfish',
+    'strawberries',
+    'tomatoes',
+    'chocolate',
+    'pollen',
+    'cats',
+];
+
+class Allergies {
+    constructor(allergenIndex) {
+        this.allergenIndex = allergenIndex;
+    }
+
+    list() {
+        return possibleAllergies.filter((allergy, i) => this.allergenIndex & Math.pow(2, i));
+    }
+
+    allergicTo(food) {
+        return this.list().some(allergy => allergy === food);
+    }
+}
+
+export default Allergies;

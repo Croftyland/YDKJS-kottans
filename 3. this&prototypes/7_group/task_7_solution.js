af68b000adac5ffcaf6da3b57bbe401cfcae22f9
Christina Landvytovych <Crofty.land@gmail.com>
2020-02-20 17:56:46 +0200

    add 8 task

--- 3. this&prototypes/7_group/task_7_solution.js
+++ 3. this&prototypes/7_group/task_7_solution.js
@@ -1,35 +1,73 @@
-function SnakesLadders() {
-    this.snakes = {16:6,46:25,49:11,62:19,64:60,74:53,89:68,92:88,95:75,99:80};
-    this.ladders = {2:38,7:14,8:31,15:26,21:42,28:84,36:44,51:67,71:91,78:98,87:94};
-    this.playerPlaces = {1:0,2:0};
-    this.currentPlayer = 1;
-    this.gameOver = false;
+class Dinglemouse {
+
+    constructor() {
+        this.name = this.age = this.sex = 0
+    }
+
+    setAge(age) {
+        this.age = age
+        return this
+    }
+
+    setSex(sex) {
+        this.sex = sex
+        return this
+    }
+
+    setName(name) {
+        this.name = name
+        return this
+    }
+
+    hello() {
+        return `Hello. My name is ${this.name}. I am ${this.age}. I am ${this.sex == 'M' ? "male" : "female"}.`
+    }
+
 }
-SnakesLadders.prototype.play = function(die1, die2) {
-    //detect playing after game over
-    if (this.gameOver) return "Game over!";
 
-    //calculate normal move
-    this.playerPlaces[this.currentPlayer] += die1+die2;
+/*solution*/
 
-    //check win condition
-    if (this.playerPlaces[this.currentPlayer] == 100) {
-        this.gameOver = true;
-        return "Player "+ this.currentPlayer +" Wins!";
-    } else if (this.playerPlaces[this.currentPlayer] > 100)
-    //handle bounce-back
-        this.playerPlaces[this.currentPlayer] = 100 - (this.playerPlaces[this.currentPlayer] - 100);
-
-    //handle landing on a snake
-    if (this.snakes[this.playerPlaces[this.currentPlayer]]) this.playerPlaces[this.currentPlayer] = this.snakes[this.playerPlaces[this.currentPlayer]];
-
-    //handle landing on a ladder
-    if (this.ladders[this.playerPlaces[this.currentPlayer]]) this.playerPlaces[this.currentPlayer] = this.ladders[this.playerPlaces[this.currentPlayer]];
-
-    var result = "Player "+this.currentPlayer+" is on square "+this.playerPlaces[this.currentPlayer];
-
-    //change players only if the two dice are different
-    if (die1 !== die2) this.currentPlayer = this.currentPlayer == 1? 2 : 1;
-
-    return result;
-};
+class Dinglemouse {
+    constructor() {
+        this.age=true
+        this.name=true
+        this.sex=true
+        this.agePos=0
+        this.namePos=0
+        this.sexPos=0
+        this.arr=[]
+    }
+    setAge(age) {
+        if (this.age){
+            this.age=false
+            this.agePos=this.arr.length
+            this.arr[this.agePos]=` I am ${age}.`
+        } else {
+            this.arr[this.agePos]=` I am ${age}.`
+        }
+        return this
+    }
+    setSex(sex) {
+        if (this.sex){
+            this.sex=false
+            this.sexPos=this.arr.length
+            this.arr[this.sexPos]=` I am ${sex == 'M' ? "male" : "female"}.`
+        } else {
+            this.arr[this.sexPos]=` I am ${sex == 'M' ? "male" : "female"}.`
+        }
+        return this
+    }
+    setName(name) {
+        if (this.name){
+            this.name=false
+            this.namePos=this.arr.length
+            this.arr[this.namePos]=` My name is ${name}.`
+        } else {
+            this.arr[this.namePos]=` My name is ${name}.`
+        }
+        return this
+    }
+    hello() {
+        return `Hello.${this.arr.join``}`
+    }
+}
--- 3. this&prototypes/7_group/task_7_test.js
+++ 3. this&prototypes/7_group/task_7_test.js
@@ -1,5 +1,25 @@
-var game = new SnakesLadders();
-Test.assertEquals(game.play(1, 1), "Player 1 is on square 38", "Should return: 'Player 1 is on square 38'");
-Test.assertEquals(game.play(1, 5), "Player 1 is on square 44", "Should return: 'Player 1 is on square 44'");
-Test.assertEquals(game.play(6, 2), "Player 2 is on square 31", "Should return: 'Player 2 is on square 31'");
-Test.assertEquals(game.play(1, 1), "Player 1 is on square 25", "Should return: 'Player 1 is on square 25'");
+describe("ExampleTests", function(){
+    it("testBob27Male", function(){
+        let dm = new Dinglemouse().setName("Bob").setAge(27).setSex('M')
+        let expected = "Hello. My name is Bob. I am 27. I am male."
+        doTest(expected, dm.hello())
+    })
+
+    it("test27MaleBob", function(){
+        let dm = new Dinglemouse().setAge(27).setSex('M').setName("Bob")
+        let expected = "Hello. I am 27. I am male. My name is Bob."
+        doTest(expected, dm.hello())
+    })
+
+    it("testAliceFemale", function(){
+        let dm = new Dinglemouse().setName("Alice").setSex('F')
+        let expected = "Hello. My name is Alice. I am female."
+        doTest(expected, dm.hello())
+    })
+
+    it("testBatman", function(){
+        let dm = new Dinglemouse().setName("Batman")
+        let expected = "Hello. My name is Batman."
+        doTest(expected, dm.hello())
+    })
+})
